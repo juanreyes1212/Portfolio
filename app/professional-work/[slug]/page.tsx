44f962cedc2d4f7ddc684ce081b0f8c33bc726d4
Juan Reyes <reyes1212@gmail.com>
2025-07-18 13:08:39 -0700

    Clean up repo - Rm legacy code, elevate Next project to root

--- app/professional-work/[slug]/page.tsx
+++ app/professional-work/[slug]/page.tsx
@@ -11,15 +11,15 @@ export async function generateStaticParams() {
 }
 
 export default function ProjectPage({ params }: { params: { slug: string } }) {
-    const { slug } = React.use(params); //@TODO: params should be awaited before using this, come back and iterate
+    const { slug } = React.use(params); //@TODO:slug  = React.use(params); //@TODO: params should be awaited before using this, come back and iterate
 
     // Find the project data based on the slug from the URL
     const project = projects.find(p => p.slug === slug);
 
     // If no project is found, show a 404 page
-    if (!project) {
-        notFound();
-    }
+    // if (!project) {
+    //     notFound();
+    // }
 
     return <ProjectDetail project={project} />;
 }
