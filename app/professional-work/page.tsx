0acca666f2ed6f858a4275828070bd6a4fcc33ee
Juan Reyes <reyes1212@gmail.com>
2025-07-20 09:17:05 -0700

    Add bio to home page, adjust work page header text

--- app/professional-work/page.tsx
+++ app/professional-work/page.tsx
@@ -6,8 +6,9 @@ export default function ProfessionalWorkPage() {
     return (
         <div>
             <h1 className="text-4xl font-bold mb-8 text-slate-900 dark:text-white">
-                Professional Work
+                Work
             </h1>
+            <p className="text-slate-600 dark:text-slate-400 mt-2 max-w-3xl"></p>
             <ProjectList projectGroups={projects} />
         </div>
     );
