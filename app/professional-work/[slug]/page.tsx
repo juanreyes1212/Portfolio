5b31c72a74816abaa65319c2ff94079d68e0b718
Juan Reyes <reyes1212@gmail.com>
2025-07-20 20:04:12 -0700

    Add new personal-projects page and slug route, new types in project data, update project details dynamic routes, update home page to bio

--- app/professional-work/[slug]/page.tsx
+++ app/professional-work/[slug]/page.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ProjectDetail from '@/components/ProjectDetail';
-import { projects } from '@/data/projects';
+import { professionalWorkData } from '@/data/professionalWorkData';
 import { notFound } from 'next/navigation';
 
 export async function generateStaticParams() {
-    return projects.map((project) => ({
+    return professionalWorkData.map((project) => ({
         slug: project.slug,
     }));
 }
 
-export default function ProjectPage({ params }: { params: { slug: string } }) {
+export default function professionalWorkPage({ params }: { params: { slug: string } }) {
     const { slug } = params; 
 
     // Find the project data based on the slug from the URL
-    const project = projects.find(p => p.slug === slug);
+    const project = professionalWorkData.find(p => p.slug === slug);
 
     if (!project) {
         notFound(); // 404 page
